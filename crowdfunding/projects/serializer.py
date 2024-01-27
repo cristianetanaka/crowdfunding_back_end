1a6b5e5bfcfa232fc49e20b134af2673a7f051d7
cristianetanaka <cristianeoshirotanaka@gmail.com>
2024-01-27 16:41:13 +0800

    pledgesmodelimplemented

--- crowdfunding/projects/serializer.py
+++ crowdfunding/projects/serializer.py
@@ -8,6 +8,10 @@ class PledgeSerializer(serializers.ModelSerializer):
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = apps.get_model('projects.Project')
         fields = '__all__'
+
+class ProjectDetailSerializer(ProjectSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
